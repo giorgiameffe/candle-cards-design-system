d0d9043a0919a211167054aee7a43f904a4d96de
giorgiameffe <giorgiameffe03@gmail.com>
2026-02-26 17:43:35 +0100

    feat: modify readme and create folders

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import './button.css';
+import "../styles/button.css"
 
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { Button } from './Button';
+import { Button } from "../components/Button"
 
 const meta: Meta<typeof Button> = {
 
