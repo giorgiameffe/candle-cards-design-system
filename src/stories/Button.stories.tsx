6bb927f3fadc178a84b35adab0877210425850f1
giorgiameffe <giorgiameffe03@gmail.com>
2026-02-26 23:15:48 +0100

    refactor(storybook): simplify Button story to its essential version

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -9,40 +9,14 @@ const meta: Meta<typeof Button> = {
   parameters: {
     layout: 'centered',
   },
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-
+  tags: ['autodocs']
 }
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
+export const Default: Story = {
 
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+  render: () => <Button />
+}
