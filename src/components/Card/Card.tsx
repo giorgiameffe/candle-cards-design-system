5708ffd59d15f74ac89b65d181ec86ca60d808f9
giorgiameffe <giorgiameffe03@gmail.com>
2026-03-16 12:24:20 +0100

    feat: implement Card component with image, badges, and button and add Storybook story for Lavander candle

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,63 @@
-export const Card: React.FC = () => {
+import { Badge } from "../Badge/Badge";
+import { Button } from "../Button/Button";
 
-    return <div>Card</div>
+export type CardProps = {
+    image: string;
+    title: string;
+    description: string;
+    price: string;
+    category: "floral" | "sweet" | "citrus";
+    status?: "new" | "discount" | "soldOut";
+}
+
+export const Card = ({
+    image,
+    title,
+    description,
+    price,
+    category,
+    status
+
+}: CardProps) => {
+
+    const isSoldOut = status === "soldOut";
+
+    return (
+
+        <article>
+
+            {/* Header della Card */}
+            <div>
+                {status && (
+                    <Badge
+                        variant={status}
+                        label={status === "discount" ? "- 20%" : status} />
+                )}
+
+                <img src={image} alt={title} />
+            </div>
+
+            {/* Body della Card */}
+            <div>
+                <Badge
+                    variant={category}
+                    label={category}
+                />
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <div>{price}</div>
+            </div>
+
+            {/* Footer della card */}
+            <div>
+                <Button
+                    label="Aggiungi al carrello"
+                    size="medium"
+                    disabled={isSoldOut}
+                />
+            </div>
+
+        </article>
+    )
+
 }
--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,8 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { Card } from "../components/Card/Card";
+import lavanderCandle from "../assets/img/candle-lavanda.jpg";
 
 const meta: Meta<typeof Card> = {
+
     // Posizione del componente nella sidebar di Storybook
     title: "Components/Molecules/Card",
     // Componente associato alla storia
@@ -19,7 +21,12 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-
-    render: () => <Card />
+export const LavanderDefault: Story = {
+    args: {
+        image: lavanderCandle,
+        title: "Lavanda di Provenza",
+        description: "Olio essenziale purissimo per conciliare il relax e il riposo notturno.",
+        price: "14,90 €",
+        category: "floral"
+    }
 }
